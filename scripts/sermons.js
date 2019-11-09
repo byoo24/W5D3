3bece188739c949b564454c3e501ee11763c2d2e
byoo24 <byoo.dev@gmail.com>
2019-11-09 17:22:58 -0500

    adjustments to sermons javascript

--- scripts/sermons.js
+++ scripts/sermons.js
@@ -1,2 +1,11 @@
-const sermonIndex = document.querySelector('#sermons-index');
-sermonIndex.scrollIntoView({ block: "start" });
+function sermonsJumpToIndex() {
+    const sermonIndex = document.querySelector('#sermons-index');
+    sermonIndex.scrollIntoView({ block: "start" });
+}
+
+
+if (document.readyState === 'loading') {  // Loading hasn't finished yet
+    document.addEventListener('DOMContentLoaded', sermonsJumpToIndex);
+} else {  // `DOMContentLoaded` has already fired
+    sermonsJumpToIndex();
+}
