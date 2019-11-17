cd4ed1f7284c0ee5b1a4a54db663f3f8c7047243
byoo24 <byoo.dev@gmail.com>
2019-11-17 16:36:46 -0500

    adjustments to timeline

--- scripts/hope_images.js
+++ scripts/hope_images.js
@@ -3,9 +3,16 @@ const hasHopeImageIndex = document.querySelector('.has-image-index');
 if(hasHopeImageIndex){
     console.log("ENTERED HOPE INDEX");
 
-    const imageIndex = hasHopeImageIndex.querySelectorAll('.hope-image-wrapper');
-    imageIndex.forEach(image => {
-        console.log(image);
+    const imageParents = hasHopeImageIndex.querySelectorAll('.hope-image-wrapper');
+    imageParents.forEach(parent => {
+        let parentWidth = parent.clientWidth;
+        let parentHeight = parent.clientHeight;
+        const img = parent.querySelector('img');
+        let imgWidth = img.clientWidth;
+        let imgHeight = img.clientHeight;
+
+
+        console.log(parentWidth, parentHeight, imgWidth, imgHeight);
     })
 
 }
