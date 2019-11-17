b2fa0597e4f4a4d01eef6e878db282b6e71c91d5
byoo24 <byoo.dev@gmail.com>
2019-11-17 18:03:53 -0500

    adjustments to timeline

--- scripts/hope_images.js
+++ scripts/hope_images.js
@@ -5,36 +5,47 @@ const STATE = {
 }
 
 
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    const hasHopeImageIndex = document.querySelector(STATE.hasIndex);
     
-    if(hasHopeImageIndex){
-        console.log("ENTERED HOPE INDEX");
-    
-        const imageParents = hasHopeImageIndex.querySelectorAll(STATE.parentClass);
-        imageParents.forEach(parent => {
-            let parentWidth = parent.clientWidth;
-            let parentHeight = parent.clientHeight;
-            const img = parent.querySelector(STATE.childClass);
-            let imgWidth = img.clientWidth;
-            let imgHeight = img.clientHeight;
-            let focalPoints = img.getAttribute('data-image-focal-point').split(',');
-            let focalX = focalPoints[0];
-            let focalY = focalPoints[1];
 
+    const centerImages = function () {
+        const hasHopeImageIndex = document.querySelector(STATE.hasIndex);
 
-            // (original height / original width) x new width = new height
-            let newWidth = parentWidth;
-            let newHeight = (imgHeight / imgWidth) * newWidth;
+        if (hasHopeImageIndex !== undefined) {
+            console.log("ENTERED HOPE INDEX");
+            const imageParents = hasHopeImageIndex.querySelectorAll(STATE.parentClass);
 
-            img.style.position = "relative";
-            img.style.width = `${newWidth}px`;
-            img.style.height = `${newHeight}px`;
-    
-            console.log("imgWidth:", imgWidth, "imgHeight:", imgHeight);
+            imageParents.forEach(parent => {
+                const img = parent.querySelector(STATE.childClass);
+                let parentWidth = parent.clientWidth;
+                let parentHeight = parent.clientHeight;
+                let imgWidth = img.clientWidth;
+                let imgHeight = img.clientHeight;
 
-        })
-    
+                let dataFocalPoints = img.dataset.image - focal - point.split(',');
+                // let dataFocalPoints = img.getAttribute('data-image-focal-point').split(',');
+                let focalX = dataFocalPoints[0];
+                let focalY = dataFocalPoints[1];
+                console.log("focalX:", focalX, "focalY:", focalY);
+
+                let dataImageDimensions = img.dataset.image-dimensions;
+                console.log("dataImageDimensions:", dataImageDimensions);
+
+                // (original height / original width) x new width = new height
+                let newWidth = parentWidth;
+                let newHeight = (imgHeight / imgWidth) * newWidth;
+
+                img.style.position = "relative";
+                img.style.width = `${newWidth}px`;
+                img.style.height = `${newHeight}px`;
+
+                console.log("imgWidth:", imgWidth, "imgHeight:", imgHeight);
+
+            })
+        }
     }
 })
 
