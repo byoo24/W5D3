6d481468e12fbb7274f52c557cfd857642a28f87
byoo24 <byoo.dev@gmail.com>
2019-11-17 17:07:52 -0500

    adjustments to timeline

--- scripts/hope_images.js
+++ scripts/hope_images.js
@@ -4,21 +4,25 @@ const STATE = {
     childClass: '.image-item'
 }
 
-const hasHopeImageIndex = document.querySelector(STATE.hasIndex);
 
-if(hasHopeImageIndex){
-    console.log("ENTERED HOPE INDEX");
+document.addEventListener('DOMContentLoaded', () => {
+    const hasHopeImageIndex = document.querySelector(STATE.hasIndex);
+    
+    if(hasHopeImageIndex){
+        console.log("ENTERED HOPE INDEX");
+    
+        const imageParents = hasHopeImageIndex.querySelectorAll(STATE.parentClass);
+        imageParents.forEach(parent => {
+            let parentWidth = parent.clientWidth;
+            let parentHeight = parent.clientHeight;
+            const img = parent.querySelector(STATE.childClass);
+            let imgWidth = img.offsetWidth;
+            let imgHeight = img.offsetHeight;
+    
+    
+            console.log("img.offsetWidth:", imgWidth, "img.offsetHeight:", imgHeight, "img.width:", img.width, "img.height:", img.height);
+        })
+    
+    }
+})
 
-    const imageParents = hasHopeImageIndex.querySelectorAll(STATE.parentClass);
-    imageParents.forEach(parent => {
-        let parentWidth = parent.clientWidth;
-        let parentHeight = parent.clientHeight;
-        const img = parent.querySelector(STATE.childClass);
-        let imgWidth = img.offsetWidth;
-        let imgHeight = img.offsetHeight;
-
-
-        console.log("img.offsetWidth:", imgWidth, "img.offsetHeight:", imgHeight, "img.width:", img.width, "img.height:", img.height);
-    })
-
-}
