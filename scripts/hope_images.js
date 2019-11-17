586c34a92ebf90608923b3031a0de9d8f8b8e9db
byoo24 <byoo.dev@gmail.com>
2019-11-17 18:19:26 -0500

    adjustments to timeline

--- scripts/hope_images.js
+++ scripts/hope_images.js
@@ -22,16 +22,17 @@ document.addEventListener('DOMContentLoaded', () => {
                 const img = parent.querySelector(STATE.childClass);
                 let parentWidth = parent.clientWidth;
                 let parentHeight = parent.clientHeight;
-                let imgWidth = img.clientWidth;
-                let imgHeight = img.clientHeight;
+                
 
                 let dataFocalPoints = img.getAttribute('data-image-focal-point').split(',');
                 let focalX = parseFloat(dataFocalPoints[0]);
                 let focalY = parseFloat(dataFocalPoints[1]);
                 console.log("focalX:", focalX, "focalY:", focalY);
 
-                let dataImageDimensions = img.getAttribute("data-image-dimensions");
-                console.log("dataImageDimensions:", dataImageDimensions);
+                let dataImageDimensions = img.getAttribute("data-image-dimensions").split('x');
+                let imgWidth = parseFloat(dataImageDimensions[0]);
+                let imgHeight = parseFloat(dataImageDimensions[1]);
+                console.log("imgWidth:", imgWidth, "imgHeight:", imgHeight);
 
                 // (original height / original width) x new width = new height
                 let newWidth = parentWidth;
@@ -40,8 +41,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 img.style.position = "relative";
                 img.style.width = `${newWidth}px`;
                 img.style.height = `${newHeight}px`;
+                console.log("newWidth:", newWidth, "newHeight:", newHeight);
 
-                console.log("imgWidth:", imgWidth, "imgHeight:", imgHeight);
 
             })
         }
