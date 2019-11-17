8f8f7c0ad42d43a304b34f21f8d4f147fc4664ae
byoo24 <byoo.dev@gmail.com>
2019-11-17 16:41:11 -0500

    adjustments to timeline

--- scripts/hope_images.js
+++ scripts/hope_images.js
@@ -1,13 +1,19 @@
-const hasHopeImageIndex = document.querySelector('.has-image-index');
+const STATE = {
+    hasIndex: ".has-image-index",
+    parentClass: '.image-wrapper',
+    childClass: '.image-item'
+}
+
+const hasHopeImageIndex = document.querySelector(STATE.hasIndex);
 
 if(hasHopeImageIndex){
     console.log("ENTERED HOPE INDEX");
 
-    const imageParents = hasHopeImageIndex.querySelectorAll('.hope-image-wrapper');
+    const imageParents = hasHopeImageIndex.querySelectorAll(STATE.parentClass);
     imageParents.forEach(parent => {
         let parentWidth = parent.clientWidth;
         let parentHeight = parent.clientHeight;
-        const img = parent.querySelector('img');
+        const img = parent.querySelector(STATE.childClass);
         let imgWidth = img.clientWidth;
         let imgHeight = img.clientHeight;
 
