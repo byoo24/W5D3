655d66da0fe2207061863706dfbf4145d8203abe
byoo24 <byoo.dev@gmail.com>
2019-11-17 18:36:36 -0500

    adjustments to timeline

--- scripts/hope_images.js
+++ scripts/hope_images.js
@@ -5,49 +5,62 @@ const STATE = {
 }
 
 
+const centerImage = (parent) => {
+    const child = parent.querySelector('img');
+
+    let parentWidth = parent.clientWidth;
+    let parentHeight = parent.clientHeight;
+
+    let dataFocalPoints = child.getAttribute('data-image-focal-point').split(',');
+    let focalX = parseFloat(dataFocalPoints[0]) || 0.5;
+    let focalY = parseFloat(dataFocalPoints[1]) || 0.5;
+    console.log("focalX:", focalX, "focalY:", focalY);
+
+    let dataImageDimensions = child.getAttribute("data-image-dimensions").split('x');
+    let childWidth = parseFloat(dataImageDimensions[0]) || parentWidth;
+    let childHeight = parseFloat(dataImageDimensions[1]) || parentHeight;
+    console.log("childWidth:", childWidth, "childHeight:", childHeight);
+
+    // (original height / original width) x new width = new height
+    let newWidth = parentWidth;
+    let newHeight = (childHeight / childWidth) * newWidth;
+
+    child.style.position = "relative";
+    child.style.width = `${newWidth}px`;
+    child.style.height = `${newHeight}px`;
+    console.log("newWidth:", newWidth, "newHeight:", newHeight);
+
+    let newTop = (newHeight * focalY) - (childHeight * focalY);
+    child.style.top = `${newTop}px`;
+    console.log(newTop);
+}
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
     
+    const hasHopeImageIndex = document.querySelector(STATE.hasIndex);
 
-    const centerImages = function () {
-        const hasHopeImageIndex = document.querySelector(STATE.hasIndex);
+    if (hasHopeImageIndex !== undefined) {
+        console.log("ENTERED HOPE INDEX");
+        const imageParents = hasHopeImageIndex.querySelectorAll(STATE.parentClass);
 
-        if (hasHopeImageIndex !== undefined) {
-            console.log("ENTERED HOPE INDEX");
-            const imageParents = hasHopeImageIndex.querySelectorAll(STATE.parentClass);
+        imageParents.forEach(parent => {
+            
+            
+            centerImage(parent);
+            
 
-            imageParents.forEach(parent => {
-                const img = parent.querySelector(STATE.childClass);
-                let parentWidth = parent.clientWidth;
-                let parentHeight = parent.clientHeight;
-                
+            
 
-                let dataFocalPoints = img.getAttribute('data-image-focal-point').split(',');
-                let focalX = parseFloat(dataFocalPoints[0]);
-                let focalY = parseFloat(dataFocalPoints[1]);
-                console.log("focalX:", focalX, "focalY:", focalY);
+            
 
-                let dataImageDimensions = img.getAttribute("data-image-dimensions").split('x');
-                let imgWidth = parseFloat(dataImageDimensions[0]);
-                let imgHeight = parseFloat(dataImageDimensions[1]);
-                console.log("imgWidth:", imgWidth, "imgHeight:", imgHeight);
-
-                // (original height / original width) x new width = new height
-                let newWidth = parentWidth;
-                let newHeight = (imgHeight / imgWidth) * newWidth;
-
-                img.style.position = "relative";
-                img.style.width = `${newWidth}px`;
-                img.style.height = `${newHeight}px`;
-                console.log("newWidth:", newWidth, "newHeight:", newHeight);
+            
 
 
-            })
-        }
+        })
     }
+    
 
-    centerImages();
 })
 
