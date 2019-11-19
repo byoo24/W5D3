85f4dea69da2581359035e5b7e9d95946930b04d
byoo24 <byoo.dev@gmail.com>
2019-11-19 10:36:24 -0500

    adjustments to hope images

--- scripts/hope_images.js
+++ scripts/hope_images.js
@@ -1,48 +1,70 @@
-const STATE = {
-    hasIndex: ".has-image-index",
-    parentClass: '.hope-image-wrapper',
-    childClass: '.image-item',
+const CONSTANT = {
+    hasHopeImage: ".has_hope_image",
+    thumbnailSearch: '.hope_thumbnail',
+    wrapperSearch: '.image_image-wrapper',
+    imgSearch: '.image_item',
     extendedWidth: 1.2
 }
 
 
 
 
-const centerImage = (parent) => {
-    const child = parent.querySelector('img');
-    // const childSrc = parent.getAttribute('data-src');
-    // child.src = childSrc;
+const centerThumbnailImages = (thumbnail) => {
     console.log('RESIZED');
+    const imgWrapper = thumbnail.querySelector(CONSTANT.wrapperSearch);
+    const img = thumbnail.querySelector(CONSTANT.imgSearch);
+    // const child = thumbnail.querySelector('img');
 
-    let parentWidth = parent.clientWidth;
-    let parentHeight = parent.clientHeight;
+    const STATE = {};
 
-    let dataFocalPoints = child.getAttribute('data-image-focal-point').split(',');
-    let focalX = parseFloat(dataFocalPoints[0]) || 0.5;
-    let focalY = parseFloat(dataFocalPoints[1]) || 0.5;
-    console.log("focalX:", focalX, "focalY:", focalY);
+    STATE.thumbnailWidth = thumbnail.clientWidth;
+    STATE.thumbnailHeight = thumbnail.clientHeight;
 
-    let dataImageDimensions = child.getAttribute("data-image-dimensions").split('x');
-    let childWidth = parseInt(dataImageDimensions[0]) || parentWidth;
-    let childHeight = parseInt(dataImageDimensions[1]) || parentHeight;
-    console.log("childWidth:", childWidth, "childHeight:", childHeight);
+    let dataFocalPoints = img.getAttribute('data-image-focal-point').split(',');
+    let focalX = parseFloat(dataFocalPoints[0]);
+    let focalY = parseFloat(dataFocalPoints[1]);
+    STATE.focalX = focalX !== NaN ? focalX : 0.5;
+    STATE.focalY = focalY !== NaN ? focalY : 0.5;
+    console.log("focalX:", STATE.focalX, "focalY:", STATE.focalY);
+
+    STATE.wrapperWidth = imgWrapper.clientWidth;
+    STATE.wrapperHeight = imgWrapper.clientHeight;
+    console.log("wrapperWidth:", STATE.wrapperWidth, "wrapperHeight:", STATE.wrapperHeight);
+
+    let dataImageDimensions = img.getAttribute("data-image-dimensions").split('x');
+    STATE.imgWidth = parseInt(dataImageDimensions[0]);
+    STATE.imgHeight = parseInt(dataImageDimensions[1]);
+    console.log("imgWidth:", STATE.imgWidth, "imgHeight:", STATE.imgHeight);
+
+        if (STATE.imgWidth === NaN) {
+            console.log('invalid image width');
+            return;
+        }
+
+        if (STATE.imgHeight === NaN) {
+            console.log('invalid image height');
+            return;
+        }
 
     // (original height / original width) x new width = new height
-    let newWidth = parentWidth;
-    let newHeight = (childHeight / childWidth) * newWidth;
-    let innerWidth = parentWidth / STATE.extendedWidth;
+    let newWidth = STATE.wrapperWidth;
+    let newHeight = (STATE.imgHeight / STATE.imgWidth) * newWidth;
 
-    child.style.position = "relative";
-    child.style.width = `${newWidth}px`;
-    child.style.height = `${newHeight}px`;
+    img.style.position = "relative";
+    img.style.width = `${newWidth}px`;
+    img.style.height = `${newHeight}px`;
     console.log("newWidth:", newWidth, "newHeight:", newHeight);
 
-    let newTop = (parentHeight * focalY) - (newHeight * focalY) || 0;
-    child.style.top = `${newTop}px`;
+    let newTop = (STATE.thumbnailHeight * focalY) - (newHeight * focalY);
+        newTop = newTop >= 0 ? newTop : 0;
+        newTop = newTop <= STATE.thumbnailHeight ? newTop : STATE.thumbnailHeight;
+    img.style.top = `${newTop}px`;
     console.log(newTop);
 
-    let newLeft = (newWidth * focalX) - (innerWidth * focalX) || 0;
-    child.style.left = `${newLeft}px`;
+    let newLeft = (newWidth * focalX) - (STATE.thumbnailWidth * focalX);
+        newLeft = newLeft >= 0 ? newLeft : 0;
+        newLeft = newLeft <= STATE.thumbnailWidth ? newLeft : STATE.thumbnailWidth; 
+    img.style.left = `${newLeft}px`;
     console.log(newLeft);
 }
 
@@ -50,15 +72,15 @@ const centerImage = (parent) => {
 
 document.addEventListener('DOMContentLoaded', () => {
     
-    const hasHopeImageIndex = document.querySelector(STATE.hasIndex);
+    const hasHopeImage = document.querySelector(CONSTANT.hasHopeImage);
 
-    if (hasHopeImageIndex !== undefined) {
+    if (hasHopeImage) {
         console.log("ENTERED HOPE INDEX");
-        const imageParents = hasHopeImageIndex.querySelectorAll(STATE.parentClass);
+        const thumbnails = hasHopeImage.querySelectorAll(CONSTANT.hope_thumbnail);
 
-        imageParents.forEach(parent => {        
-            centerImage(parent);
-            window.addEventListener('resize', function(){centerImage(parent)});
+        thumbnails.forEach(thumbnail => {
+            centerThumbnailImages(thumbnail);
+            window.addEventListener('resize', function(){ centerThumbnailImages(thumbnail) });
         })
     }
 })
